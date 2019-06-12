c1aa97322bbb2865d1eed77ca4ca497682673788
Ravikiran303 <ravikiran2221@gmail.com>
2019-06-12 21:42:26 +0530

    Add proper object

--- Day-1/Person/Person.java
+++ Day-1/Person/Person.java
@@ -1,19 +1,19 @@
-class Person {
+class PersonDemo {
     public static void main(String[] args) {
-        Details name_assign = new Details();
-        name_assign.setName("Ravi");
-        System.out.println(name_assign.getName());
+        Person person = new Person();
+        person.setName("Ravi");
+        System.out.println(person.getName());
     }
 }
 
-class Details {
-    String Person_Name;
+class Person {
+    String name;
 
     void setName(String name) {
-        Person_Name = name;
+        this.name = name;
     }
 
     String getName() {
-        return Person_Name;
+        return name;
     }
 }
