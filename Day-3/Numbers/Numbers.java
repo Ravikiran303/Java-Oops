13f5b182e7c36afbe5bb039cc00847c1d46c7e0b
Ravikiran303 <ravikiran2221@gmail.com>
2019-06-16 12:39:36 +0530

    Print 1 to 100 numbers

---
+++ Day-3/Numbers/Numbers.java
@@ -0,0 +1,17 @@
+class NumbersDemo {
+    public static void main(String[] args) {
+        Numbers numbers = new Numbers();
+        numbers.PrintNumbers();
+    }
+}
+
+class Numbers {
+    int num;
+
+    void PrintNumbers() {
+        for (num = 1; num <= 100; num++) {
+            System.out.print(num + " ");
+        }
+
+    }
+}
