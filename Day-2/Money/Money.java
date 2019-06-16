4b7fcfdc50bf2500502168a48b40f3ce15818c23
Ravikiran303 <ravikiran2221@gmail.com>
2019-06-16 11:43:35 +0530

    Money object comparision and addition

--- Day-2/Money/Money.java
+++ Day-2/Money/Money.java
@@ -1,8 +1,34 @@
 class MoneyDemo {
     public static void main(String[] args) {
-
+        Money money = new Money();
+        money.setValue(5);
+        Money money1 = new Money();
+        money1.setValue(5);
+        System.out.println(money.equals(money1));
+        System.out.println(money.addition(money1));
     }
 }
 
 class Money {
+    int value;
+
+    void setValue(int value) {
+        this.value = value;
+    }
+
+    public boolean equals(Object o) {
+        Money money = (Money) o;
+        if (money == null) {
+            return false;
+        }
+        if (!(o instanceof Money)) {
+            return false;
+        }
+        return this.value == money.value;
+    }
+
+    int addition(Money money) {
+        return this.value + money.value;
+    }
+
 }
